4d3f7a5ad7d93496dd093734daabc9fe26b29df4
Anas Arfeen <codecrusader07@gmail.com>
2025-12-19 03:41:09 +0530

    web

--- frontend/scripts/data.js
+++ frontend/scripts/data.js
@@ -1,9 +1,103 @@
 window.SYMPTOMS_BY_REGION = {
-  "Head": ["Headache", "Dizziness"],
-  "Chest": ["Chest pain", "Breathlessness"],
-  "Abdomen": ["Stomach pain", "Nausea"],
-  "Left Arm": ["Pain", "Weakness"],
-  "Right Arm": ["Pain", "Numbness"],
-  "Left Leg": ["Pain", "Swelling"],
-  "Right Leg": ["Pain", "Cramps"]
+"Head": [
+    "Headache",
+    "Dizziness",
+    "Pain behind the eyes",
+    "Sinus pressure",
+    "Loss of balance",
+    "Loss of smell",
+    "Visual disturbances",
+    "Blurred and distorted vision",
+    "Slurred speech"
+  ],
+  "Throat": [
+    "Throat irritation",
+    "Ulcers on tongue",
+    "Patches in throat"
+  ],
+  "Chest": [
+    "Chest pain",
+    "Breathlessness",
+    "Palpitations",
+    "Fast heart rate"
+  ],
+  "Abdomen": [
+    "Abdominal pain",
+    "Stomach pain",
+    "Belly pain",
+    "Indigestion",
+    "Distention of abdomen",
+    "Pain during bowel movements",
+    "Passage of gases",
+    "Constipation",
+    "Diarrhoea",
+    "Nausea",
+    "Vomiting",
+    "Stomach bleeding",
+    "Loss of appetite",
+    "Swelling of stomach"
+  ],
+  "Pelvis": [
+    "Bladder discomfort",
+    "Burning micturition",
+    "Spotting urination",
+    "Foul smell of urine",
+    "Continuous feel of urine"
+  ],
+  "Neck": [
+    "Neck pain",
+    "Stiff neck",
+    "Movement stiffness"
+  ],
+  "Back": [
+    "Back pain"
+  ],
+  "Left Arm": [
+    "Muscle pain",
+    "Muscle weakness",
+    "Joint pain"
+  ],
+  "Right Arm": [
+    "Muscle pain",
+    "Muscle weakness",
+    "Joint pain"
+  ],
+  "Left Leg": [
+    "Hip joint pain",
+    "Knee pain",
+    "Joint pain",
+    "Muscle pain",
+    "Muscle weakness",
+    "Muscle wasting",
+    "Cramps",
+    "Weakness in limbs",
+    "Painful walking"
+  ],
+  "Right Leg": [
+    "Hip joint pain",
+    "Knee pain",
+    "Joint pain",
+    "Muscle pain",
+    "Muscle weakness",
+    "Muscle wasting",
+    "Cramps",
+    "Weakness in limbs",
+    "Painful walking"
+  ],
+  "Skin": [
+    "Skin rash",
+    "Itching",
+    "Dischromic patches",
+    "Skin peeling",
+    "Redness of eyes"
+  ],
+  "General": [
+    "Fatigue",
+    "Lethargy",
+    "Malaise",
+    "Sweating",
+    "Chills",
+    "Shivering",
+    "Dehydration"
+  ]
 };
